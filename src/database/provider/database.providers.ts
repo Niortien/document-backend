3bbf4b593ce27c58c3e2857dae46104df146940a
niortien <niortiencoulibaly2001@gmail.com>
2026-09-10 05:56:15 +0000

    iicc

--- src/database/provider/database.providers.ts
+++ src/database/provider/database.providers.ts
@@ -7,11 +7,15 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT ?? '5432', 10),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        ...(process.env.DATABASE_URL
+          ? { url: process.env.DATABASE_URL }
+          : {
+              host: process.env.DB_HOST,
+              port: parseInt(process.env.DB_PORT ?? '5432', 10),
+              username: process.env.DB_USERNAME,
+              password: process.env.DB_PASSWORD,
+              database: process.env.DB_NAME,
+            }),
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
